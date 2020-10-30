8f4780673042a43e7bb22e6f715c90a86d63deb4
trevorbonas <trevorbonas@gmail.com>
2020-10-29 22:14:27 -0700

    Attempt to start edit keyboard

--- app/src/main/java/com/raspberry/practicalparent/EditFragment.java
+++ app/src/main/java/com/raspberry/practicalparent/EditFragment.java
@@ -1,5 +1,6 @@
 package com.raspberry.practicalparent;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import android.app.Dialog;
@@ -14,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -30,7 +32,8 @@ import com.raspberry.practicalparent.model.KidManager;
 public class EditFragment extends AppCompatDialogFragment {
 
     private int index;
-
+    @Override
+    @Nullable
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         View v = LayoutInflater.from(getActivity()).inflate(R.layout.edit_layout, null);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -61,6 +64,10 @@ public class EditFragment extends AppCompatDialogFragment {
                 name.setEnabled(true);
                 name.setInputType(InputType.TYPE_CLASS_TEXT);
                 name.setSelected(true);
+                name.requestFocus();
+                InputMethodManager imm = (InputMethodManager) getActivity()
+                        .getSystemService(Context.INPUT_METHOD_SERVICE);
+
                 name.setOnEditorActionListener(new TextView.OnEditorActionListener() {
                     @Override
                     public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
@@ -71,6 +78,7 @@ public class EditFragment extends AppCompatDialogFragment {
                                 String newName = name.getText().toString();
                                 kids.getKidAt(index).setName(newName);
                                 ((KidOptionsActivity)getActivity()).setupListView();
+                                name.clearFocus();
                                 dismiss();
                                 return true;
                         }
@@ -92,8 +100,6 @@ public class EditFragment extends AppCompatDialogFragment {
         // Build the alert dialog
 
         Dialog d = builder.setView(v).create();
-        d.getWindow().setSoftInputMode(WindowManager
-                .LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
         return d;
 
