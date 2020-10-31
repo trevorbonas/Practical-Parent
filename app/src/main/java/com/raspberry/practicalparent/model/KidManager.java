cb47b281c360fea62d5a724e38ddaa57f5fe7929
trevorbonas <trevorbonas@gmail.com>
2020-10-31 14:26:51 -0700

    Fix delete kid conflict

--- app/src/main/java/com/raspberry/practicalparent/model/KidManager.java
+++ app/src/main/java/com/raspberry/practicalparent/model/KidManager.java
@@ -39,7 +39,6 @@ public class KidManager {
         if (i < 0 || i >= kids.size()) {
             return;
         }
-        kids.remove(i);
         // If the kid we just deleted was our current kid (whose turn it is)
         // go to the next kid
         if (i == currentIndex && kids.size() > 1) {
