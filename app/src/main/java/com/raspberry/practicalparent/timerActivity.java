80c3d0be3e672c3663fa40f115ace2de0f4a7d99
Shubham <Shubhamn@sfu.ca>
2020-10-31 14:56:47 -0700

    added custom timer capability

--- app/src/main/java/com/raspberry/practicalparent/timerActivity.java
+++ app/src/main/java/com/raspberry/practicalparent/timerActivity.java
@@ -1,6 +1,5 @@
 package com.raspberry.practicalparent;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,21 +7,26 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
 public class timerActivity extends AppCompatActivity {
 
-    private static final long START_TIME_IN_MILLIS = 600000;
 
+    private EditText mEditTextInput;
+    private Button mButtonSet;
 
     private TextView mTextViewCountDown;
     private Button mButtonStartPause;
     private Button mButtonReset;
     private CountDownTimer mCountDownTimer;
     private boolean mTimerRunning;
+    private long mStartTimeInMillis;
     private long mTimeLeftInMillis;
     private long mEndTime;
 
@@ -32,10 +36,37 @@ public class timerActivity extends AppCompatActivity {
         long startTimer = intent.getLongExtra(timerActivityMainMenu.EXTRA_INT, 0);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
-            
+
+        //setting time in minutes
+        mEditTextInput = findViewById(R.id.edit_text_input);
         mTextViewCountDown = findViewById(R.id.text_view_countdown);
+
+        mButtonSet = findViewById(R.id.button_set);
         mButtonStartPause = findViewById(R.id.button_start_pause);
         mButtonReset = findViewById(R.id.button_reset);
+
+        mButtonSet.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String input = mEditTextInput.getText().toString();
+                if(input.length() == 0) {
+                    Toast.makeText(timerActivity.this, "Field can't be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                long millisInput = Long.parseLong(input) * 60000;
+
+                if(millisInput == 0){
+                    Toast.makeText(timerActivity.this, "Please enter a positive number", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                setTime(millisInput);
+                mEditTextInput.setText("");
+            }
+        });
+
+
         
         mButtonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,6 +88,13 @@ public class timerActivity extends AppCompatActivity {
 
     }
 
+    //setting custom time
+    private void setTime(long milliseconds) {
+        mStartTimeInMillis = milliseconds;
+        resetTimer();
+        closeKeyboard();
+    }
+
     private void startTimer() {
         mEndTime = System.currentTimeMillis() + mTimeLeftInMillis;
         mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
@@ -72,13 +110,13 @@ public class timerActivity extends AppCompatActivity {
 //                mButtonStartPause.setText("Start");
 //                mButtonStartPause.setVisibility(View.INVISIBLE);
 //                mButtonReset.setVisibility(View.VISIBLE);
-                updateButtons();
+                updateWatchInterface();
                 //TODO: add android notification
             }
         }.start();
 
         mTimerRunning = true;
-        updateButtons();
+        updateWatchInterface();
     }
 
 
@@ -86,28 +124,39 @@ public class timerActivity extends AppCompatActivity {
     private void pauseTimer() {
         mCountDownTimer.cancel();
         mTimerRunning = false;
-        updateButtons();
+        updateWatchInterface();
     }
     
     private  void resetTimer() {
-        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        mTimeLeftInMillis = mStartTimeInMillis;
         updateCountDownText();
-        updateButtons();
+        updateWatchInterface();
     }
 
     private void updateCountDownText() {
-        int minutes = (int) (mTimeLeftInMillis / 1000) / 60; //turns millis to mins
+        int hours = (int) (mTimeLeftInMillis / 1000) / 3600; //turns hours to mins
+        int minutes = (int) ((mTimeLeftInMillis / 1000) % 3600)/ 60; //turns millis to mins
         int seconds = (int) (mTimeLeftInMillis / 1000) % 60; //turns millis to secs
-        String timeLeftFormated = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
-
+        String timeLeftFormated;
+        if(hours > 0){
+            timeLeftFormated = String.format(Locale.getDefault(),
+                    "%d:%02d:%02d", hours,  minutes, seconds);
+        } else {
+            timeLeftFormated = String.format(Locale.getDefault(),
+                    "%02d:%02d", minutes, seconds);
+        }
         mTextViewCountDown.setText(timeLeftFormated);
     }
 
-    private void updateButtons() {
+    private void updateWatchInterface() {
         if (mTimerRunning) {
+            mEditTextInput.setVisibility(View.INVISIBLE);
+            mButtonSet.setVisibility(View.INVISIBLE);  //set button and text input to invisible
             mButtonReset.setVisibility(View.INVISIBLE);
             mButtonStartPause.setText("Pause");
         } else {
+            mEditTextInput.setVisibility(View.VISIBLE);
+            mButtonSet.setVisibility(View.VISIBLE);  //set button and text input to visible
             mButtonStartPause.setText("Start");
 
             if(mTimeLeftInMillis < 1000) {
@@ -116,7 +165,7 @@ public class timerActivity extends AppCompatActivity {
                 mButtonStartPause.setVisibility(View.VISIBLE);
             }
 
-            if(mTimeLeftInMillis < START_TIME_IN_MILLIS) {
+            if(mTimeLeftInMillis < mStartTimeInMillis) {
                 mButtonReset.setVisibility(View.VISIBLE);
             } else {
                 mButtonReset.setVisibility(View.INVISIBLE);
@@ -124,7 +173,14 @@ public class timerActivity extends AppCompatActivity {
         }
     }
 
-
+    // close keyboard after setting timer
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if(view != null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
 
 
     //for timer to run in background
@@ -136,24 +192,30 @@ public class timerActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = prefs.edit();
 
         //TODO: change strings to constants
+        editor.putLong("startTimeInMillis", mStartTimeInMillis);
         editor.putLong("millisLeft", mTimeLeftInMillis);
         editor.putBoolean("timerRunning", mTimerRunning);
         editor.putLong("endTime", mEndTime);
 
         editor.apply();
-        mCountDownTimer.cancel(); //cancel timer when time runs out
+
+        if(mCountDownTimer != null){
+            mCountDownTimer.cancel(); //cancel timer when time runs out
+        }
     }
+
 
     @Override
     protected void onStart() {
         super.onStart();
 
         SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-        mTimeLeftInMillis = prefs.getLong("millisLeft", START_TIME_IN_MILLIS);
+        mStartTimeInMillis = prefs.getLong("startTimeInMillis", 600000);
+        mTimeLeftInMillis = prefs.getLong("millisLeft", mStartTimeInMillis);
         mTimerRunning = prefs.getBoolean("timerRunning", false);
 
         updateCountDownText();
-        updateButtons();
+        updateWatchInterface();
 
         if (mTimerRunning){
             mEndTime = prefs.getLong("endTime", 0);
@@ -164,7 +226,7 @@ public class timerActivity extends AppCompatActivity {
                 mTimeLeftInMillis = 0;
                 mTimerRunning = false;
                 updateCountDownText();
-                updateButtons();  //make buttons invisible
+                updateWatchInterface();  //make buttons invisible
             } else {
                 startTimer();
             }
