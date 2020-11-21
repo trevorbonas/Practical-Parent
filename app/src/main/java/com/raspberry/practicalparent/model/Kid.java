ac8a425b6de0e8297ab410612aa50b20f36f851b
trevorbonas <trevorbonas@gmail.com>
2020-11-20 19:51:46 -0800

    Add filename String to Kid.class

--- app/src/main/java/com/raspberry/practicalparent/model/Kid.java
+++ app/src/main/java/com/raspberry/practicalparent/model/Kid.java
@@ -1,5 +1,7 @@
 package com.raspberry.practicalparent.model;
 
+import android.graphics.Bitmap;
+
 /**
  * A Kid class that holds a given kid's name and age
  */
@@ -7,14 +9,28 @@ public class Kid {
 
     private int age; // Optional feature
     private String name;
+    private String filename; // Filename of the kid's portrait
 
     public Kid(String name) {
         this.name = name;
+        filename = "None"; // Default, no profile pic
     }
 
     public void setName(String name) {
         this.name = name;
     }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getFilename(){
+        return filename;
+    }
+
+    /*public Bitmap getPortrait() {
+        // Get the portrait
+    }*/
 
     public void setAge(int age) {
         if (age < 0 || age > 18) {
